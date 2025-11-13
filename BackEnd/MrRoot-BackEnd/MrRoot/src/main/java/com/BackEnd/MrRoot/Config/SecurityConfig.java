4ab90103f30a4ea30ecc4daf509c5515f9b07d9b
2001lakindu <lakindu72001@gmail.com>
2025-11-13 10:48:50 +0530

    add security config file to allow API for interact with server

---
+++ BackEnd/MrRoot-BackEnd/MrRoot/src/main/java/com/BackEnd/MrRoot/Config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.BackEnd.MrRoot.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/distance/graph").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
+
+
+
+
