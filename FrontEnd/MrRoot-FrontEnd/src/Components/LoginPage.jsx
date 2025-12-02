7a32042fb496eda7cb24f09c02111f6ea8599de0
2001lakindu <lakindu72001@gmail.com>
2025-12-02 08:49:36 +0530

    add OAuth template ui to login the application

---
+++ FrontEnd/MrRoot-FrontEnd/src/Components/LoginPage.jsx
@@ -0,0 +1,28 @@
+import "../Leaflet/Loginpage.css";
+
+export default function LoginPage() {
+
+    const connectToGoogleServer=()=>{
+       window.location.href="http://localhost:8080/auth/google";
+    }
+
+  return (
+    <div className="full-page">
+      <div className="signin-part">
+        <h1>Sign in with Google</h1>
+      </div>
+
+      <div className="login-part">
+        <button className="login-button" onClick={connectToGoogleServer}>
+  <img
+    src="https://www.svgrepo.com/show/475656/google-color.svg"
+    alt="Google logo"
+    className="google-icon"
+  />
+  Login with Google
+</button>
+
+      </div>
+    </div>
+  );
+}
