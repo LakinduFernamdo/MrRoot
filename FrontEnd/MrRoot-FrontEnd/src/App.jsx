c6797f6e317e7f4f5b21408aef6b5ea6b877b5be
2001lakindu <lakindu72001@gmail.com>
2025-12-03 10:40:10 +0530

    protected MapUI page with save jwt token in local storage

--- FrontEnd/MrRoot-FrontEnd/src/App.jsx
+++ FrontEnd/MrRoot-FrontEnd/src/App.jsx
@@ -1,18 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from "./Components/LoginPage";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import MapUI from "./Components/MapUI";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         {/* Default login page */}
         <Route path="/" element={<LoginPage />} />
 
         {/* Map route after Google login */}
-        <Route path="/map" element={<MapUI />} />
+        <Route
+          path="/map"
+          element={
+            <ProtectedRoute>
+              <MapUI />
+            </ProtectedRoute>
+          }
+        />
+        
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
